ec8099fbde3f1c68794be3356bb28df0c2a85854
MrJakeSir <roarba011@gmail.com>
2021-06-19 12:25:27 -0600

    XD

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 from discord.ext import commands
 from discord import Embed, Intents
+from json import load
 
-VERSION = "1.0.0"
+with open("bot.json") as bot_info:
+     bot = load(bot_info)
+     token = bot["TOKEN"]
+     prefix = commands.when_mentioned_or(bot["PREFIX"])
+     VERSION = bot["VERSION"]
 
 it = Intents().all()
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or("="),
+client = commands.Bot(command_prefix=prefix,
                       help_command=None,
                       description="Bot mas troll que conocerás",
                       intents=it)
@@ -40,6 +45,8 @@ async def ping(ctx : commands.Context):
      await ctx.reply(embed=embed, mention_author=False)
 
 
+
+
 if __name__ == "__main__":
      
 #    Carga todas las extensiones
@@ -47,5 +54,4 @@ if __name__ == "__main__":
           client.load_extension(name=cog)
 
 #    Carga el token del bot
-     # client.run("ODUwMDY4ODQ1MjE2Mzk5NDcw.YLkWTg.C6shAU5Q2z29spoWSd8pF2QziYI")
-     client.run("ODU0MTY2NzQwNDk4MjUxNzc3.YMf-xA.ea5xJOooFdv3Tj11X32U58Ad6jo")
+     client.run(token)
